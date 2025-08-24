e65d8cac777f044512c08999fe73d777fcd784ca
kumudha24 <kumudhashree2004@gmail.com>
2025-08-24 17:32:57 +0530

    Update

--- plantaid-frontend/src/components/PlantAid.jsx
+++ plantaid-frontend/src/components/PlantAid.jsx
@@ -91,7 +91,7 @@ function PlantAid() {
             auto-rotate="true"
             auto-rotate-delay="1000"
             rotation-per-second="30deg"
-            camera-orbit="0deg 90deg 300m"
+            camera-orbit="0deg 90deg 350m"
             shadow-intensity="1"
             shadow-softness="0.5"
             style={modelViewerStyle}
